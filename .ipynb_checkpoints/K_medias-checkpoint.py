065cd9626056471faa9881233830fa327dcdad42
Isabel Solano <misolanob@gmail.com>
2023-03-21 00:57:52 -0600

    Implementación sin librerías

---
+++ .ipynb_checkpoints/K_medias-checkpoint.py
@@ -0,0 +1,7 @@
+import pandas as pd # Import Pandas for data manipulation using dataframes
+import numpy as np # Import Numpy for data statistical analysis 
+import matplotlib.pyplot as plt # Import matplotlib for data visualisation
+import seaborn as sns # Statistical data visualization
+from ydata_profiling import ProfileReport
+from quickda.clean_data import *
+
